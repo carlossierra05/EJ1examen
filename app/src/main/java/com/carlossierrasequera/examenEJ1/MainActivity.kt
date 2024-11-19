09d4e91eec3721ed7e4b8f0fa1b7845e62198ea7
carlossierra05 <carlosss30112005@gmail.com>
2024-11-19 10:19:12 +0100

    a

--- app/src/main/java/com/carlossierrasequera/examenEJ1/MainActivity.kt
+++ app/src/main/java/com/carlossierrasequera/examenEJ1/MainActivity.kt
@@ -4,28 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.carlossierrasequera.examenEJ1.ui.theme.EJ1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            EJ1Theme {
                listaVideojuegos()
             }
         }
     }
-}
 
 @Composable
 fun VideojuegoItem(url: String, nombre: String, precio: String) {
